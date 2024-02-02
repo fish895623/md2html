c09768af6ed84f1e264f67242a2ba3950317f3bf
fish895623 <dan990429@gmail.com>
2024-02-03 02:12:06 +0900

    Add dependencies and update regex version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use log::info;
+use regex::Regex;
 use std::io::Result;
 
 #[cfg(not(tarpaulin))]
@@ -8,21 +8,42 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn parse_header_context(context: String) -> Result<String> {
-    info!("Parsing header context: {}", context);
+fn parse_header_context(context: String) -> Result<Vec<(String, String)>> {
     let return_value = context;
+    let asdf = Regex::new(r"^#+\s(.*)$").unwrap();
 
-    Ok(return_value)
+    let mut results = Vec::new();
+    for (_, [path, line]) in asdf
+        .captures_iter(return_value.as_str())
+        .map(|c| c.extract())
+    {
+        results.push((path.to_string(), line.to_string()));
+    }
+
+    Ok(results)
 }
 
 #[cfg(test)]
-mod main_test {
+mod tests {
     use super::*;
+    use log::info;
 
+    fn init_logger() {
+        let _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .is_test(true)
+            .try_init();
+    }
     #[test]
     fn test_parse_header_context() {
+        init_logger();
+
+        info!("Hello World");
         let context = "## Hello World";
         let result = parse_header_context(context.to_string()).unwrap();
-        assert_eq!(result, "Hello World");
+
+        for (path, line) in result {
+            info!("Path: {}, Line: {}", path, line);
+        }
     }
 }
