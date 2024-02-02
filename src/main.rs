c98f6a3d01c509c2dc853d6e895531929e1d97b4
fish895623 <dan990429@gmail.com>
2024-02-03 02:39:48 +0900

    Refactor parse_header_context function to use a struct for context representation

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,26 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn parse_header_context(context: String) -> Result<Vec<(String, String)>> {
+struct Context {
+    level: i128,
+    text: String,
+}
+
+fn parse_header_context(context: String) -> Result<Vec<Context>> {
     let return_value = context;
-    let asdf = Regex::new(r"^#+\s(.*)$").unwrap();
+    let asdf = Regex::new(r"^(#+)\s(.*)$").unwrap();
 
     let mut results = Vec::new();
     for (_, [path, line]) in asdf
         .captures_iter(return_value.as_str())
         .map(|c| c.extract())
     {
-        results.push((path.to_string(), line.to_string()));
+        let level = path.to_string().len();
+
+        results.push(Context {
+            level: level as i128,
+            text: line.to_string(),
+        });
     }
 
     Ok(results)
@@ -25,8 +35,9 @@ fn parse_header_context(context: String) -> Result<Vec<(String, String)>> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use log::info;
+
+    use super::*;
 
     fn init_logger() {
         let _ = env_logger::builder()
@@ -37,13 +48,13 @@ mod tests {
     #[test]
     fn test_parse_header_context() {
         init_logger();
-
-        info!("Hello World");
+        info!("test_parse_header_context");
         let context = "## Hello World";
         let result = parse_header_context(context.to_string()).unwrap();
 
-        for (path, line) in result {
-            info!("Path: {}, Line: {}", path, line);
+        for context in result {
+            assert_eq!(context.level, 2);
+            assert_eq!(context.text, "Hello World");
         }
     }
 }
