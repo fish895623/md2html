4f78b3dd8f3ca34eb0bf9598ff774680f5de42af
fish895623 <dan990429@gmail.com>
2024-02-03 03:13:27 +0900

    Add rustfmt.toml and reformat code

--- src/main.rs
+++ src/main.rs
@@ -3,58 +3,58 @@ use std::io::Result;
 
 #[cfg(not(tarpaulin))]
 fn main() -> Result<()> {
-    println!("Hello, world!");
+  println!("Hello, world!");
 
-    Ok(())
+  Ok(())
 }
 
 struct Context {
-    level: i128,
-    text: String,
+  level: i128,
+  text: String,
 }
 
 fn parse_header_context(context: String) -> Result<Vec<Context>> {
-    let return_value = context;
-    let asdf = Regex::new(r"^(#+)\s(.*)$").unwrap();
+  let return_value = context;
+  let asdf = Regex::new(r"^(#+)\s(.*)$").unwrap();
 
-    let mut results = Vec::new();
-    for (_, [path, line]) in asdf
-        .captures_iter(return_value.as_str())
-        .map(|c| c.extract())
-    {
-        let level = path.to_string().len();
+  let mut results = Vec::new();
+  for (_, [path, line]) in asdf
+    .captures_iter(return_value.as_str())
+    .map(|c| c.extract())
+  {
+    let level = path.to_string().len();
 
-        results.push(Context {
-            level: level as i128,
-            text: line.to_string(),
-        });
-    }
+    results.push(Context {
+      level: level as i128,
+      text: line.to_string(),
+    });
+  }
 
-    Ok(results)
+  Ok(results)
 }
 
 #[cfg(test)]
-mod tests {
-    use log::info;
+mod main_tests {
+  use log::info;
 
-    use super::*;
+  use super::*;
 
-    fn init_logger() {
-        let _ = env_logger::builder()
-            .filter_level(log::LevelFilter::max())
-            .is_test(true)
-            .try_init();
+  fn init_logger() {
+    let _ = env_logger::builder()
+      .filter_level(log::LevelFilter::max())
+      .is_test(true)
+      .try_init();
+  }
+  #[test]
+  fn test_parse_header_context() {
+    init_logger();
+    info!("test_parse_header_context");
+    let context = "## Hello World";
+    let result = parse_header_context(context.to_string()).unwrap();
+
+    for context in result {
+      assert_eq!(context.level, 2);
+      assert_eq!(context.text, "Hello World");
     }
-    #[test]
-    fn test_parse_header_context() {
-        init_logger();
-        info!("test_parse_header_context");
-        let context = "## Hello World";
-        let result = parse_header_context(context.to_string()).unwrap();
-
-        for context in result {
-            assert_eq!(context.level, 2);
-            assert_eq!(context.text, "Hello World");
-        }
-    }
+  }
 }
