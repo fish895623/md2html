562ec7a6c52ca92f80f28ab25af9b7b08aaef7ce
배성훈 <dan990429@gmail.com>
2024-02-05 13:56:17 +0900

    Add version endpoint to Flask app

--- pymd2html/main.py
+++ pymd2html/main.py
@@ -5,5 +5,14 @@ from pymd2html import __version__
 app = Flask("pymd2html")
 
 
+@app.route("/version")
+def get_version():
+    return __version__
+
+
 def main():
     app.run()
+
+
+if __name__ == "__main__":
+    main()
